f9dd7e1e811cbaef3b9e4f0340f022d24fd7ec45
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2021-01-07 16:18:28 +0100

    log ud

--- app/src/main/java/madspild/Fragments/ProfileFragment.java
+++ app/src/main/java/madspild/Fragments/ProfileFragment.java
@@ -2,6 +2,7 @@ package madspild.Fragments;
 
 import androidx.fragment.app.Fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,10 +10,23 @@ import android.view.ViewGroup;
 
 import com.example.madspild.R;
 
+import java.util.Objects;
+
+import madspild.Activities.MainActivity;
+import madspild.Activities.StartActivity;
+import madspild.Helpers.HttpClientHelper;
+
 public class ProfileFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        view.findViewById(R.id.logout_button).setOnClickListener((event) -> {
+            HttpClientHelper.removeToken();
+            Intent intent = new Intent(getActivity(), StartActivity.class);
+            startActivity(intent);
+            Objects.requireNonNull(getActivity()).finish();
+        });
+        return view;
     }
 }
